f30fc7476d69fc131f85a5b99fa3d121a2b5fd82
Mona Reijneke <cooljay.mr@gmail.com>
2023-08-29 21:42:43 +0200

    category vergelijk

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,60 +1,58 @@
 import {
-  SimpleGrid,
+  Text,
   CardFooter,
-  ListItem,
   Heading,
   Flex,
   Card,
   CardHeader,
   CardBody,
-  Text,
   Button,
-  HStack,
-  Tag,
-  TagLabel,
   Image,
-  Divider,
-  Spacer,
+  HStack,
 } from "@chakra-ui/react";
 
-export const EventPage = ({ item, clickFn }) => {
-  console.log(item);
+export const EventPage = ({ item, cat, clickFn }) => {
   return (
-    <SimpleGrid
-      spacing={4}
-      templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-    >
-      <Card variant="elevated">
-        <CardHeader>
-          <Heading color="#38B2AC" size="md" align="center">
-            <Image src={item.image} borderRadius="5px" w="full" h="8em" />
-            {item.title}
-          </Heading>
-        </CardHeader>
-        <CardBody>
-          <Text fontSize="sm">{item.description}</Text>
-        </CardBody>
-        <CardFooter>
-          <Flex spacing="5px" flexDir="row">
-            <Button
-              color="#38B2Ay"
-              borderRadius={50}
-              align="bottom"
-              onClick={() => clickFn()}
-            >
-              back
-            </Button>
-            <Button
-              color="#38B2A"
-              borderRadius={50}
-              align="bottom"
-              onClick={() => clickFn()}
-            >
-              delete
-            </Button>
-          </Flex>
-        </CardFooter>
-      </Card>
-    </SimpleGrid>
+    <Flex flexDirection={{ base: "column", sm: "row" }}>
+      <Text fontSize={{ base: "24px", sm: "40px", md: "56px" }}>
+        <Card backgroundColor="gray.100" variant="elevated">
+          <CardHeader>
+            <Heading color="#38B2AC" size="md" align="center">
+              <Image src={item.image} borderRadius="5px" w="full" h="8em" />
+              {item.title}
+              <span>
+                {cat.map(
+                  category => category.id === item.find(id => id.categoryIds)
+                )
+               return category.name}
+              </span>
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <Text fontSize="sm">{item.description}</Text>
+          </CardBody>
+          <CardFooter>
+            <HStack spacing="auto">
+              <Button
+                color="#38B2Ay"
+                borderRadius={5}
+                align="bottom"
+                onClick={() => clickFn()}
+              >
+                back
+              </Button>
+              <Button
+                color="#38B2A"
+                borderRadius={5}
+                align="bottom"
+                onClick={() => clickFn()}
+              >
+                delete
+              </Button>
+            </HStack>
+          </CardFooter>
+        </Card>
+      </Text>
+    </Flex>
   );
 };
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -6,29 +6,29 @@ import { Heading, Flex, Wrap, Center } from "@chakra-ui/react";
 
 export const loader = async () => {
   const events = await fetch("http://localhost:3000/events");
-  // const categories = await fetch("http://localhost:3000/categories");
-  // const users = await fetch("http://localhost:3000/users");
+  const categories = await fetch("http://localhost:3000/categories");
+  //const users = await fetch("http://localhost:3000/users");
 
   return {
     events: await events.json(),
-    // categories: await categories.json(),
-    // users: await users.json(),
+    categories: await categories.json(),
+    //users: await users.json(),
   };
 };
 
 export const EventsPage = ({ clickFn }) => {
-  const { events } = useLoaderData();
+  const { events, categories } = useLoaderData();
   const [searchField, setSearchField] = useState("");
 
   const handleChange = event => {
     setSearchField(event.target.value);
   };
   return (
-    <Flex flexDirection={"column"} p="-1.5">
+    <Flex flexDirection={"column"}>
       <TextInput
         item={searchField}
         changeFn={handleChange}
-        width="300px"
+        width="15em"
         mb={10}
       />
 
@@ -38,7 +38,12 @@ export const EventsPage = ({ clickFn }) => {
       <Wrap spacing="10px">
         <Center>
           {events.map(event => (
-            <EventPage clickFn={clickFn} item={event} key={event.id} />
+            <EventPage
+              clickFn={clickFn}
+              item={event}
+              cat={categories}
+              key={event.id}
+            />
           ))}
         </Center>
       </Wrap>
